7dc59cf20f041bf0300f52065b20b3a9880f3eff
gamusiro <gamusiro310@icloud.com>
2022-10-07 12:09:03 +0900

    画面クリアまで

--- Graphics.cpp
+++ Graphics.cpp
@@ -3,6 +3,8 @@
 // Description:
 // Copyright (C) 2022 Silicon Studio Co., Ltd. All rights reserved.
 //==============================================================================
+#include <vector>
+
 #include "Graphics.h"
 using namespace Microsoft::WRL;
 
@@ -16,6 +18,91 @@ Graphics* Graphics::Get()
 
 // 初期化処理
 bool Graphics::Init(const int width, const int height, const HWND hWnd)
+{
+	if (!this->CreateDeviceAndSwapChain(width, height, hWnd))
+	{// デバイス・スワップチェインの生成に失敗
+		return false;
+	}
+
+	if (!this->CreateRenderTargetView())
+	{// レンダーターゲットの生成に失敗
+		return false;
+	}
+
+	if (!this->CreateFence())
+	{// フェンスの生成に失敗
+		return false;
+	}
+
+
+	return true;
+}
+
+// 終了処理
+void Graphics::Uninit()
+{
+}
+
+// 画面クリア
+void Graphics::Clear()
+{
+	// 現在のバッファのインデックスを取得
+	UINT index = m_swapChain->GetCurrentBackBufferIndex();
+	this->SetResourceBarrier(
+		index,
+		D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_PRESENT,		
+		D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_RENDER_TARGET	// 今からレンダーターゲットとして使用
+	);
+
+	// レンダーターゲットの指定
+	D3D12_CPU_DESCRIPTOR_HANDLE renderTargetHeapHandle = m_renderTargetViewHeap->GetCPUDescriptorHandleForHeapStart();
+	renderTargetHeapHandle.ptr += index * m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	m_commandList->OMSetRenderTargets(1, &renderTargetHeapHandle, false, nullptr);
+
+	float clearColor[]{ 0.0f, 0.5f, 0.0f, 1.0f };
+	m_commandList->ClearRenderTargetView(renderTargetHeapHandle, clearColor, 0, nullptr);	// 画面クリアコマンドの発行
+}
+
+// バッファ切り替え 描画コマンド実行
+void Graphics::Present()
+{
+	// 現在のバッファのインデックスを取得
+	UINT index = m_swapChain->GetCurrentBackBufferIndex();
+
+	this->SetResourceBarrier(
+		index,
+		D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_RENDER_TARGET,		// 前の状態がレンダーターゲット
+		D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_PRESENT				
+	);
+
+	// コマンド命令を閉じる
+	m_commandList->Close();
+
+	// コマンドリストの実行
+	std::vector<ID3D12CommandList*> commandLists{ m_commandList.Get() };
+	m_commandQueue->ExecuteCommandLists(commandLists.size(), &commandLists[0]);
+
+	// キューの命令処理が終了するまで待つ
+	m_commandQueue->Signal(m_fence.Get(), ++m_fenceValue);
+
+	if (m_fence->GetCompletedValue() != m_fenceValue)
+	{
+		HANDLE fenceEvent = CreateEvent(nullptr, false, false, nullptr);
+		m_fence->SetEventOnCompletion(m_fenceValue, fenceEvent);
+		WaitForSingleObject(fenceEvent, INFINITE);
+		CloseHandle(fenceEvent);
+	}
+
+	// コマンド・命令のリセット
+	m_commandAllocator->Reset();
+	m_commandList->Reset(m_commandAllocator.Get(), nullptr);
+
+	// フリップ
+	m_swapChain->Present(1, 0);
+}
+
+// デバイスとスワップチェインの生成
+bool Graphics::CreateDeviceAndSwapChain(const int width, const int height, const HWND hWnd)
 {
 	HRESULT ret{};
 
@@ -94,7 +181,7 @@ bool Graphics::Init(const int width, const int height, const HWND hWnd)
 	swapChainDesc.SampleDesc.Count		= 1;
 	swapChainDesc.SampleDesc.Quality	= 0;
 	swapChainDesc.BufferUsage			= DXGI_USAGE_BACK_BUFFER;
-	swapChainDesc.BufferCount			= k_BackBufferNum;
+	swapChainDesc.BufferCount			= k_backBufferNum;
 	swapChainDesc.Scaling				= DXGI_SCALING::DXGI_SCALING_STRETCH;
 	swapChainDesc.SwapEffect			= DXGI_SWAP_EFFECT::DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	swapChainDesc.AlphaMode				= DXGI_ALPHA_MODE::DXGI_ALPHA_MODE_UNSPECIFIED;
@@ -111,25 +198,72 @@ bool Graphics::Init(const int width, const int height, const HWND hWnd)
 	{// スワップチェインの生成に失敗
 		return false;
 	}
-	
-
 
 	return true;
 }
 
-// 終了処理
-void Graphics::Uninit()
+// レンダーターゲットの生成
+bool Graphics::CreateRenderTargetView()
 {
+	HRESULT ret{};
+
+	// レンダーターゲットの生成処理
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc{};
+	heapDesc.Type			= D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	heapDesc.NodeMask		= 0;
+	heapDesc.NumDescriptors = k_backBufferNum;
+	heapDesc.Flags			= D3D12_DESCRIPTOR_HEAP_FLAGS::D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	ret = m_device->CreateDescriptorHeap(&heapDesc, __uuidof(ID3D12DescriptorHeap), (void**)m_renderTargetViewHeap.GetAddressOf());
+	if (FAILED(ret))
+	{// ヒープの生成に失敗
+		return false;
+	}
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc{};
+	ret = m_swapChain->GetDesc(&swapChainDesc);
+	if (FAILED(ret))
+	{
+		return false;
+	}
+
+	D3D12_CPU_DESCRIPTOR_HANDLE handle = m_renderTargetViewHeap->GetCPUDescriptorHandleForHeapStart();	// ヒープの先頭アドレス取得
+	for (UINT i = 0; i < k_backBufferNum; ++i)
+	{// レンダーターゲット用のバックバッファを作成
+		ret = m_swapChain->GetBuffer(i, __uuidof(ID3D12Resource), (void**)m_backBuffers[i].GetAddressOf());
+		m_device->CreateRenderTargetView(m_backBuffers[i].Get(), nullptr, handle);
+		handle.ptr += m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_RTV);	// RTV1つ分のサイズ分をインクリメント
+	}
+
+	return true;
 }
 
-// 画面クリア
-void Graphics::Clear()
+// フェンスの生成
+bool Graphics::CreateFence()
 {
-	
+	HRESULT ret{};
+	ret = m_device->CreateFence(
+		m_fenceValue,
+		D3D12_FENCE_FLAGS::D3D12_FENCE_FLAG_NONE,
+		__uuidof(ID3D12Fence),
+		(void**)m_fence.GetAddressOf()
+	);
+	if (FAILED(ret))
+	{// フェンスの生成に失敗
+		return false;
+	}
+
+	return true;
 }
 
-// バッファ切り替え 描画コマンド実行
-void Graphics::Present()
+// リソースバリアの設定
+void Graphics::SetResourceBarrier(const UINT index, D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after)
 {
-	
+	D3D12_RESOURCE_BARRIER barrierDesc{};
+	barrierDesc.Type					= D3D12_RESOURCE_BARRIER_TYPE::D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+	barrierDesc.Flags					= D3D12_RESOURCE_BARRIER_FLAGS::D3D12_RESOURCE_BARRIER_FLAG_NONE;
+	barrierDesc.Transition.pResource	= m_backBuffers[index].Get();
+	barrierDesc.Transition.Subresource	= D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+	barrierDesc.Transition.StateBefore	= before;
+	barrierDesc.Transition.StateAfter	= after;
+	m_commandList->ResourceBarrier(1, &barrierDesc);
 }
--- Graphics.h
+++ Graphics.h
@@ -59,20 +59,85 @@ public:
 	void Present();
 
 private:
+	//---------------------------------------------
+	/// デバイスとスワップチェインの生成 
+	/// 
+	/// \param[in] ( width )
+	/// \param[in] ( height )
+	/// \param[in] ( hWnd )
+	/// 
+	/// \return	true 成功
+	//---------------------------------------------
+	bool CreateDeviceAndSwapChain(
+		/* [in] */  const int width,
+		/* [in] */  const int height,
+		/* [in] */  const HWND hWnd
+		);
+
+	//---------------------------------------------
+	/// レンダーターゲットの生成
+	/// 
+	/// \return	none
+	//---------------------------------------------
+	bool CreateRenderTargetView();
+
+	//---------------------------------------------
+	/// フェンスの生成
+	/// 
+	/// \return	none
+	//---------------------------------------------
+	bool CreateFence();
+
+	//---------------------------------------------
+	/// リソースバリアの設定
+	/// 
+	/// \param[in] ( index )
+	/// \param[in] ( before )
+	/// \param[in] ( after )
+	/// 
+	/// \return	none
+	//---------------------------------------------
+	void SetResourceBarrier(
+		/* [in] */  const UINT index,
+		/* [in] */  D3D12_RESOURCE_STATES before,
+		/* [in] */  D3D12_RESOURCE_STATES after
+	);
+
 	//--------------------------------------------------------------------------
-	static const UINT									k_BackBufferNum = 2;
+	static const UINT									k_backBufferNum = 2;
 	Microsoft::WRL::ComPtr<ID3D12Device>				m_device;
 	Microsoft::WRL::ComPtr<ID3D12CommandAllocator>		m_commandAllocator;
 	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList>	m_commandList;
 	Microsoft::WRL::ComPtr<ID3D12CommandQueue>			m_commandQueue;
-	Microsoft::WRL::ComPtr<IDXGISwapChain1>				m_swapChain;
+	Microsoft::WRL::ComPtr<IDXGISwapChain4>				m_swapChain;
 	//--------------------------------------------------------------------------
 
 	/// <summary>
-	/// ID3D12Device				m_device;			DirectX12 デバイス インタフェース
+	/// ID3D12Device				m_device;			DirectX12 デバイス
 	/// ID3D12CommandAllocator		m_commandAllocator;	DirectX12 コマンドアロケータ
 	/// ID3D12GraphicsCommandList	m_commandList;		DirectX12 コマンドリスト
 	/// ID3D12CommandQueue			m_commandQueue;		DirectX12 コマンドキュー
+	/// </summary>
+
+
+	//--------------------------------------------------------------------------
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>		m_renderTargetViewHeap;
+	Microsoft::WRL::ComPtr<ID3D12Resource>				m_backBuffers[k_backBufferNum];
+	//--------------------------------------------------------------------------
+
+	/// <summary>
+	/// ID3D12DescriptorHeap		m_renderTargetViewHeap;			DirectX12 レンダーターゲット用のヒープ領域
+	/// ID3D12Resource				m_backBuffers[k_backBufferNum]; DirectX12 レンダーターゲット用バックバッファ(ダブルバッファ)
+	/// </summary>
+	
+	//--------------------------------------------------------------------------
+	Microsoft::WRL::ComPtr<ID3D12Fence>		m_fence;
+	UINT									m_fenceValue;
+	//--------------------------------------------------------------------------
+
+	/// <summary>
+	/// ID3D12Fence					m_fence;		DirectX12 フェンス
+	/// UINT						m_fenceValue;	DirectX12 フェンスバリュー			
 	/// </summary>
 };
 
